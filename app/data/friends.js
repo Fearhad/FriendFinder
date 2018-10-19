4284ffbaafd89a5f1adc351bf91bc915fb4f7b63
Allan Hartigan <allan.hartigan@gmail.com>
2018-10-19 18:37:07 -0400

    Added more reserved friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,20 +4,85 @@
 // ===============================================================================
 
 var friendsArray = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    
+      {
+        "name":"Austin Matthews",
+        "photo":"https://images.thestar.com/qyNfDApeVNOFxwFvLiQIJBmeknU=/1086x691/smart/filters:cb(1521752128398)/https://www.thestar.com/content/dam/thestar/sports/leafs/2018/03/22/leafs-auston-matthews-nikita-zaitsev-will-return-to-lineup-against-predators/auston.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"Mitch Marner",
+        "photo":"https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Feditorinleaf.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F08%2F667255854-columbus-blue-jackets-v-toronto-maple-leafs.jpg.jpg&c=sc&w=850&h=560",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+          ]
+      },
+      {
+        "name":"John Tavares",
+        "photo":"https://images.thestar.com/QoE3ZSk1SWV0IruQ5Z2i7k5juxc=/1200x872/smart/filters:cb(1537475698533)/https://www.thestar.com/content/dam/thestar/sports/leafs/2018/09/20/john-tavares-another-advantage-for-maple-leafs-power-play/jt.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+      {
+        "name":"William Nylander",
+        "photo":"https://s.yimg.com/ny/api/res/1.2/0uYOkxqft5gpr0Zu5Yed9g--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/8840e25d028bfa20719cb631822d1339",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+          ]
+      },
+      {
+        "name":"Morgan Rielly",
+        "photo":"https://nhl.bamcontent.com/images/photos/286268976/1024x576/cut.jpg",
         "scores":[
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
           ]
       }
   ];
